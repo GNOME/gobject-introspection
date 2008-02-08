b935261f387dffa2c7006fe1be04820004810e87
Rob Taylor <rob.taylor@codethink.co.uk>
2008-02-08 15:31:03 +0000

    Added: Added: Renamed to tools/Makefile.am: Renamed to tools/compiler.c:
    2008-02-08  Rob Taylor  <rob.taylor@codethink.co.uk>
    
    	* Makefile.am:
    	* configure.ac:
    	* gidl/Makefile.am: Added:
    	* girepository/Makefile.am: Added:
    	* src/Makefile.am: Renamed to tools/Makefile.am:
    	* src/compiler.c: Renamed to tools/compiler.c:
    	* src/g-idl-offsets.pl: Renamed to tools/g-idl-offsets.pl:
    	* src/generate.c: Renamed to tools/generate.c:
    	* src/gidlmodule.c: Renamed to tools/gidlmodule.c:
    	* src/gidlmodule.h: Renamed to tools/gidlmodule.h:
    	* src/gidlnode.c: Renamed to tools/gidlnode.c:
    	* src/gidlnode.h: Renamed to tools/gidlnode.h:
    	* src/gidlparser.c: Renamed to tools/gidlparser.c:
    	* src/gidlparser.h: Renamed to tools/gidlparser.h:
    	* src/gidlwriter.c: Renamed to tools/gidlwriter.c:
    	* src/gidlwriter.h: Renamed to tools/gidlwriter.h:
    	* src/ginfo.c: Renamed to girepository/ginfo.c:
    	* src/ginvoke.c: Renamed to girepository/ginvoke.c:
    	* src/girepository.c: Renamed to girepository/girepository.c:
    	* src/girepository.h: Renamed to girepository/girepository.h:
    	* src/gmetadata.c: Renamed to girepository/gmetadata.c:
    	* src/gmetadata.h: Renamed to girepository/gmetadata.h:
    	* src/scanner.c: Renamed to tools/scanner.c:
    	* src/scanner.h: Renamed to tools/scanner.h:
    	* src/scannerlexer.l: Renamed to tools/scannerlexer.l:
    	* src/scannerparser.y: Renamed to tools/scannerparser.y:
    	* tests/invoke/Makefile.am:
    	Split src/ into girepository/ and tools/
    
    	* Makefile.am:
    	* configure.ac:
    	* girepository/Makefile.am:
    	* tests/Makefile.am:
    	* tests/invoke/Makefile.am:
    	* tests/parser/Makefile.am:
    	* tests/roundtrips.sh:
    	* tools/Makefile.am:
    	Make distcheck work.
    
    svn path=/trunk/; revision=104

--- tests/roundtrips.sh
+++ tests/roundtrips.sh
@@ -4,23 +4,23 @@ SIMPLE_TESTS="array.test boxed.test enum.test errors.test function.test interfac
 
 for i in $SIMPLE_TESTS; do
 	echo $i
-	../src/g-idl-compiler --raw $i > $i.1; 
-	../src/g-idl-generate --raw $i.1 > $i.2; 
-	diff -u $i $i.2 || exit 1; 
+	../tools/g-idl-compiler --raw $srcdir/$i > $i.1; 
+	../tools/g-idl-generate --raw $i.1 > $i.2; 
+	diff -u $srcdir/$i $i.2 || exit 1; 
 	rm $i.1 $i.2
 done
 
-../src/g-idl-compiler --raw --module=Foo object.test gobject.test > object.test.1
-../src/g-idl-generate --raw object.test.1 > object.test.2
-diff -u object.test object.test.2 || exit 1
+../tools/g-idl-compiler --raw --module=Foo $srcdir/object.test $srcdir/gobject.test > object.test.1
+../tools/g-idl-generate --raw object.test.1 > object.test.2
+diff -u $srcdir/object.test object.test.2 || exit 1
 rm object.test.1 object.test.2
 
-../src/g-idl-compiler --raw --module=Foo xref1.test xref2.test > xref1.test.1
-../src/g-idl-generate --raw xref1.test.1 > xref1.test.2
-diff -u xref1.test xref1.test.2 || exit 1
+../tools/g-idl-compiler --raw --module=Foo $srcdir/xref1.test $srcdir/xref2.test > xref1.test.1
+../tools/g-idl-generate --raw xref1.test.1 > xref1.test.2
+diff -u $srcdir/xref1.test xref1.test.2 || exit 1
 rm xref1.test.1 xref1.test.2
 
-../src/g-idl-compiler --raw --module=Bar xref1.test xref2.test > xref2.test.1
-../src/g-idl-generate --raw xref2.test.1 > xref2.test.2
-diff -u xref2.test xref2.test.2 || exit 1
+../tools/g-idl-compiler --raw --module=Bar $srcdir/xref1.test $srcdir/xref2.test > xref2.test.1
+../tools/g-idl-generate --raw xref2.test.1 > xref2.test.2
+diff -u $srcdir/xref2.test xref2.test.2 || exit 1
 rm xref2.test.1 xref2.test.2
