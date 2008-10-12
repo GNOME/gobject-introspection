5b49f52893815c63f4268b5ad27e27291aa433b7
Tommi Komulainen <tommi.komulainen@iki.fi>
2008-10-12 21:57:32 +0000

    run 'gitestrepo' in 'make check' instantiate 'GCancellable' to ensure the
    2008-10-12  Tommi Komulainen  <tommi.komulainen@iki.fi>
    
    	* tests/repository/Makefile.am: run 'gitestrepo' in 'make check'
    	* tests/repository/gitestrepo.c (main): instantiate
    	'GCancellable' to ensure the GType is registered before using
    	g_type_from_name()
    
    svn path=/trunk/; revision=699

--- tests/repository/gitestrepo.c
+++ tests/repository/gitestrepo.c
@@ -11,6 +11,7 @@ main(int argc, char **argv)
   gboolean ret;
   GError *error = NULL;
   GIBaseInfo *info;
+  GType gtype;
 
   g_type_init ();
 
@@ -22,6 +23,10 @@ main(int argc, char **argv)
 
   info = g_irepository_find_by_name (repo, "Gio", "Cancellable");
   g_assert (info != NULL);
+  g_assert (g_base_info_get_type (info) == GI_INFO_TYPE_OBJECT);
+
+  gtype = g_registered_type_info_get_g_type ((GIRegisteredTypeInfo *)info);
+  g_assert (g_type_is_a (gtype, G_TYPE_OBJECT));
 
   info = g_irepository_find_by_gtype (repo, g_type_from_name ("GCancellable"));
   g_assert (info != NULL);
