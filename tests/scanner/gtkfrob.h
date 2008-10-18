a17e0d1dcd24d1f6d6316f2d8065f25b77254669
Colin Walters <walters@src.gnome.org>
2008-10-18 00:44:19 +0000

    Add GtkFrob test that mirrors GtkSource
    svn path=/trunk/; revision=747

---
+++ tests/scanner/gtkfrob.h
@@ -0,0 +1,11 @@
+#ifndef __UTILITY_H__
+#define __UTILITY_H__
+
+#include <glib-object.h>
+
+
+/* A global function */
+
+void gtk_frob_language_manager_get_default ();
+
+#endif /* __UTILITY_H__ */
