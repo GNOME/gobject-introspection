20fcdbb2665d46180f445881b264eb3ed5ba64e9
Colin Walters <walters@src.gnome.org>
2008-11-13 19:57:07 +0000

    Add a file to include with Makefile fragments useful for scanner invocation
    svn path=/trunk/; revision=910

---
+++ common.mk
@@ -0,0 +1,10 @@
+SCANNER_BIN = $(top_srcdir)/tools/g-ir-scanner
+SCANNER_PYTHONPATH = $(top_builddir):$(top_srcdir):$$PYTHONPATH
+SCANNER_ENV = env PYTHONPATH=$(SCANNER_PYTHONPATH) \
+	UNINSTALLED_INTROSPECTION_SRCDIR=$(top_srcdir) \
+	UNINSTALLED_INTROSPECTION_BUILDDIR=$(top_builddir)
+SCANNER_ARGS = -v --add-include-path=$(top_builddir)/gir --add-include-path=.
+SCANNER = $(SCANNER_ENV) $(SCANNER_BIN) $(SCANNER_ARGS)
+SCANNER_LIBS = \
+	$(top_srcdir)/giscanner/*.py \
+	$(top_builddir)/giscanner/libgiscanner.la
