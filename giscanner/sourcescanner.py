5e1c9e463e0cecc6e1952a7dae55641850e7a87d
Colin Walters <walters@verbum.org>
2009-08-24 14:23:21 -0400

    Bug 590883 - Use 'cc' instead of 'gcc' for preprocessing
    Don't hardcode gcc, any cc which handles those options is good
    enough; in particular this is reported to fix Sun Studio.

--- giscanner/sourcescanner.py
+++ giscanner/sourcescanner.py
@@ -250,7 +250,7 @@ class SourceScanner(object):
 
         defines = ['__GI_SCANNER__']
         undefs = []
-        cpp_args = ['gcc', '-E', '-C', '-I.', '-']
+        cpp_args = ['cc', '-E', '-C', '-I.', '-']
 
         cpp_args += self._cpp_options
         proc = subprocess.Popen(cpp_args,
