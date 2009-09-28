5013e23d5405db533133a4b158d9dfa0ea95fbe4
Adam Sampson <ats-gnomebugs@offog.org>
2009-09-28 13:45:23 -0400

    Consistently resolve symbolic links
    When building the list of valid filenames for the scanner, resolve symlinks in
    the filenames; the lexer does this when including files, so otherwise we'll get
    filename mismatches if the path to the files being scanned includes a symlink.
    
    Signed-off-by: Colin Walters <walters@verbum.org>

--- giscanner/giscannermodule.c
+++ giscanner/giscannermodule.c
@@ -23,6 +23,7 @@
 #  include "config.h"
 #endif
 #include "sourcescanner.h"
+#include "grealpath.h"
 #include <Python.h>
 
 #ifdef _WIN32
@@ -335,7 +336,7 @@ pygi_source_scanner_append_filename (PyGISourceScanner *self,
     return NULL;
 
   self->scanner->filenames = g_list_append (self->scanner->filenames,
-					    g_strdup (filename));
+					    g_realpath (filename));
   
   Py_INCREF (Py_None);
   return Py_None;
