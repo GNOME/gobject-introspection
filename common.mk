c1836c4e455eb4c98f3a2a5ce97995e070cf9377
Johan Dahlin <johan@gnome.org>
2009-12-30 23:52:22 +0100

    Make compiler output with silent-rules prettier
    This is only supported with automake 1.11 or newer, older versions
    should default to showing nothing for now.

--- common.mk
+++ common.mk
@@ -4,7 +4,7 @@ SCANNER_ENV = env PYTHONPATH=$(SCANNER_PYTHONPATH) \
 	UNINSTALLED_INTROSPECTION_SRCDIR=$(top_srcdir) \
 	UNINSTALLED_INTROSPECTION_BUILDDIR=$(top_builddir)
 SCANNER_ARGS = -v --add-include-path=$(top_builddir)/gir --add-include-path=.
-SCANNER = $(AM_V_GEN) env LPATH=.libs $(CHECK_DEBUG) $(SCANNER_ENV) $(SCANNER_BIN) $(SCANNER_ARGS)
+SCANNER = env LPATH=.libs $(CHECK_DEBUG) $(SCANNER_ENV) $(SCANNER_BIN) $(SCANNER_ARGS)
 SCANNER_LIBS = \
 	$(top_srcdir)/giscanner/*.py \
 	$(top_builddir)/giscanner/libgiscanner.la \
