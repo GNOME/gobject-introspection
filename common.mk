a7e3ba116614e27af22a3f6ff6bd1788642be0b2
Johan Dahlin <johan@gnome.org>
2010-05-26 14:04:37 -0300

    [common.mk] Clean up, avoid excessive variables
    Just define the ones we need, that are actually used by
    the other makefiles.

--- common.mk
+++ common.mk
@@ -1,19 +1,24 @@
-SCANNER_BIN = $(top_builddir)/tools/g-ir-scanner
-SCANNER_PYTHONPATH = $(top_builddir):$(top_srcdir)
-SCANNER_ENV = env PYTHONPATH=$(SCANNER_PYTHONPATH) \
-	UNINSTALLED_INTROSPECTION_SRCDIR=$(top_srcdir) \
-	UNINSTALLED_INTROSPECTION_BUILDDIR=$(top_builddir)
-SCANNER_ARGS = -v --add-include-path=$(top_builddir)/gir --add-include-path=.
-SCANNER = env LPATH=.libs $(CHECK_DEBUG) $(SCANNER_ENV) $(SCANNER_BIN) $(SCANNER_ARGS)
-SCANNER_LIBS = \
-	$(top_srcdir)/giscanner/*.py \
-	$(top_builddir)/giscanner/libgiscanner.la \
-	$(top_builddir)/girepository/libgirepository-1.0.la
+# -*- Mode: make -*-
+# Copyright 2009-2010 Johan Dahlin
+#
+# This file defines variables that are compatible with
+# Makefile.introspection, but for use within the gobject-introspection
+# module itself.
+#
 
-INTROSPECTION_SCANNER = $(SCANNER)
-INTROSPECTION_SCANNER_ARGS = $(SCANNER_ARGS)
+INTROSPECTION_SCANNER = \
+    env LPATH=.libs \
+        PYTHONPATH=$(top_builddir):$(top_srcdir) \
+	UNINSTALLED_INTROSPECTION_SRCDIR=$(top_srcdir) \
+	UNINSTALLED_INTROSPECTION_BUILDDIR=$(top_builddir) \
+    $(top_builddir)/tools/g-ir-scanner$(EXEEXT)
+INTROSPECTION_SCANNER_ARGS = \
+    --verbose \
+    --add-include-path=. \
+    --add-include-path=$(top_builddir)/gir
+
 INTROSPECTION_COMPILER = $(top_builddir)/tools/g-ir-compiler$(EXEEXT)
 INTROSPECTION_COMPILER_ARGS = \
-	--includedir=$(srcdir) \
-	--includedir=$(top_srcdir)/gir \
+    --includedir=$(srcdir) \
+    --includedir=$(top_srcdir)/gir \
     --includedir=$(top_builddir)/gir
