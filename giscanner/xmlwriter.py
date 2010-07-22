7bcb292e7ff35764a02629213589f304df8b5076
Johan Dahlin <johan@gnome.org>
2010-07-22 14:43:02 -0300

    [scanner] Make it compatible with python 2.y
    https://bugzilla.gnome.org/show_bug.cgi?id=618562

--- giscanner/xmlwriter.py
+++ giscanner/xmlwriter.py
@@ -67,7 +67,7 @@ def collect_attributes(tag_name, attributes, self_indent,
     return attr_value
 
 
-with LibtoolImporter:
+with LibtoolImporter(None, None):
     from giscanner._giscanner import collect_attributes
 
 
