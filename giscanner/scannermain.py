920512de3fb9bfefffadb454308d66eca9c9ebd4
Colin Walters <walters@verbum.org>
2010-09-10 15:01:54 -0400

    scanner: Readd support for --strip-prefix
    We still really do want people to port to --identifier-prefix
    (and --symbol-prefix if necessary) because there's no ambiguity.
    Before the way we were casefolding was just a terrible mess.

--- giscanner/scannermain.py
+++ giscanner/scannermain.py
@@ -246,8 +246,9 @@ def scanner_main(args):
     libraries = options.libraries
 
     if options.strip_prefix:
-        _error("""Option --strip-prefix has been replaced;
-see --identifier-prefix and --symbol-prefix.""")
+        print """g-ir-scanner: warning: Option --strip-prefix has been deprecated;
+see --identifier-prefix and --symbol-prefix."""
+        options.identifier_prefixes.append(options.strip_prefix)
 
     filenames = []
     for arg in args:
