3ff1d7ef11a8a00a56c9ac1dcff946e3b87a8f92
Colin Walters <walters@verbum.org>
2010-09-29 15:35:14 -0400

    glib-2.0.c: Fix invalid annotation syntax

--- gir/glib-2.0.c
+++ gir/glib-2.0.c
@@ -101,7 +101,7 @@
  * g_spawn_async:
  * @working_directory: (allow-none):
  * @envp: (allow-none):
- * @child_setup: (scope async): (allow-none):
+ * @child_setup: (scope async) (allow-none):
  * @user_data: (allow-none):
  * @child_pid: (out):
  */
@@ -110,7 +110,7 @@
  * g_spawn_async_with_pipes:
  * @working_directory: (allow-none):
  * @envp: (allow-none):
- * @child_setup: (scope async): (allow-none):
+ * @child_setup: (scope async) (allow-none):
  * @user_data: (allow-none):
  * @child_pid: (out):
  * @standard_input: (out):
@@ -122,7 +122,7 @@
  * g_spawn_sync:
  * @working_directory: (allow-none):
  * @envp: (allow-none):
- * @child_setup: (scope call): (allow-none):
+ * @child_setup: (scope call) (allow-none):
  * @user_data: (allow-none):
  * @standard_output: (out):
  * @standard_error: (out):
@@ -150,7 +150,7 @@
  * g_shell_parse_argv:
  * @command_line:
  * @argcp: (out):
- * @argvp: (out): (array zero-terminated=1):
+ * @argvp: (out) (array zero-terminated=1):
  * @error:
  */
 
