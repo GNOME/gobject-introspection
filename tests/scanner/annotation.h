d15e386c8e10dd6b645a444e29cc053100089a07
Andreas Rottmann <a.rottmann@gmx.at>
2010-10-05 23:49:33 +0200

    Apply `(type bitfield)' annotations for enums
    Previously, such annotations would be ignored.

--- tests/scanner/annotation.h
+++ tests/scanner/annotation.h
@@ -4,6 +4,15 @@
 #include <glib-object.h>
 
 /**
+ * AnnotationBitfield: (type bitfield)
+ **/
+typedef enum
+{
+  ANNOTATION_FLAG_FOO = 1,
+  ANNOTATION_FLAG_BAR = 2
+} AnnotationBitfield;
+
+/**
  * AnnotationCallback:
  * @in: (in) (transfer none): array of ints
  *
