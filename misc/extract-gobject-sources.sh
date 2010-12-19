288133b2b8d22ad09686f775d9b796d58b71df7c
Pavel Holejsovsky <pholejs@src.gnome.org>
2010-12-19 10:46:22 +0100

    Fix misc/extract-gobject-sources.sh.
    g-ir-annotation-tool is in builddir root now.  Also avoid feeding
    problematic and not-GI-useful headers to it.

--- misc/extract-gobject-sources.sh
+++ misc/extract-gobject-sources.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 GLIBDIR=$1
-tools/g-ir-annotation-tool --extract \
+sources=`ls $GLIBDIR/gobject/*.c $GLIBDIR/gobject/*.h | \
+    grep -v 'gobject_trace.h' | \
+    grep -v 'stamp-'`
+./g-ir-annotation-tool --extract \
     -DGOBJECT_COMPILATION \
     -I$GLIBDIR \
     -I$GLIBDIR/glib \
     -I$GLIBDIR/gobject \
     -I$GLIBDIR/gmodule \
-    $GLIBDIR/gobject/*.c $GLIBDIR/gobject/*.h
+    $sources
