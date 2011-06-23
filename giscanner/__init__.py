158db28b46eb1e9b24705bf8e9c3e6ca69ae54b4
Colin Walters <walters@verbum.org>
2011-06-23 17:26:57 -0400

    giscanner: Support srcdir != builddir
    __path__ modification trick from Johan Dahlin.

--- giscanner/__init__.py
+++ giscanner/__init__.py
@@ -17,3 +17,8 @@
 # Free Software Foundation, Inc., 59 Temple Place - Suite 330,
 # Boston, MA 02111-1307, USA.
 #
+
+import os
+builddir = os.environ.get('UNINSTALLED_INTROSPECTION_BUILDDIR')
+if builddir is not None:
+    __path__.append(os.path.join(builddir, 'giscanner'))
