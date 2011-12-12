02410b7e36a5bc02a07e9f9661fed672715f09f8
Jasper St. Pierre <jstpierre@mecheye.net>
2011-12-12 15:24:20 -0500

    scanner: Remove duplicate declaration of ErrorQuarkFunction

--- giscanner/ast.py
+++ giscanner/ast.py
@@ -596,13 +596,6 @@ class ErrorQuarkFunction(Function):
         self.error_domain = error_domain
 
 
-class ErrorQuarkFunction(Function):
-
-    def __init__(self, name, retval, parameters, throws, symbol, error_domain):
-        Function.__init__(self, name, retval, parameters, throws, symbol)
-        self.error_domain = error_domain
-
-
 class VFunction(Callable):
 
     def __init__(self, name, retval, parameters, throws):
