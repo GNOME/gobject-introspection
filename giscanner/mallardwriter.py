99109f5a8ae11b7f182da497ca7b2d1dc3d11920
Tomeu Vizoso <tomeu.vizoso@collabora.com>
2012-02-20 13:43:30 +0100

    g-ir-doc-tool: Properly link to signal pages

--- giscanner/mallardwriter.py
+++ giscanner/mallardwriter.py
@@ -64,7 +64,14 @@ class MallardFormatter(object):
             result += self.escape(para[:pos])
             rest = para[pos + 1:]
             link = re.split('[^a-zA-Z_:-]', rest, maxsplit=1)[0]
-            if link in self._namespace.ctypes:
+            if '::' in link:
+                type_name, signal_name = link.split('::')
+                if type_name in self._namespace.ctypes:
+                    type_ = self._namespace.get_by_ctype(type_name)
+                    xref = '%s.%s-%s' % (self._namespace.name, type_.name, signal_name)
+                else:
+                    xref = link
+            elif link in self._namespace.ctypes:
                 type_ = self._namespace.get_by_ctype(link)
                 xref = '%s.%s' % (self._namespace.name, type_.name)
             else:
