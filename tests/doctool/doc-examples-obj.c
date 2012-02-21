52c07daf1fa3a5d17957cc3e3d0d75c65b268b57
Tomeu Vizoso <tomeu.vizoso@collabora.com>
2012-02-21 16:17:59 +0100

    g-ir-doc-tool: Create a GIR file specific for testing doc generation
    This will also serve as a reference of the syntax that is currently
    supported by g-ir-doc-tool.

---
+++ tests/doctool/doc-examples-obj.c
@@ -0,0 +1,103 @@
+/* doc-examples-obj.c */
+
+#include "doc-examples-obj.h"
+
+/**
+ * DocExamplesObj:
+ *
+ * This is an example of how to document a class
+ *
+ * This class has a signal: #DocExamplesObj::signal-example.
+ *
+ * And also has a property: #DocExamplesObj:property-example.
+ *
+ * Since: 0.99
+ */
+
+G_DEFINE_TYPE (DocExamplesObj, doc_examples_obj, G_TYPE_OBJECT)
+
+static void
+doc_examples_obj_get_property (GObject    *object,
+                               guint       property_id,
+                               GValue     *value,
+                               GParamSpec *pspec)
+{
+  switch (property_id)
+    {
+    default:
+      G_OBJECT_WARN_INVALID_PROPERTY_ID (object, property_id, pspec);
+    }
+}
+
+static void
+doc_examples_obj_set_property (GObject      *object,
+                               guint         property_id,
+                               const GValue *value,
+                               GParamSpec   *pspec)
+{
+  switch (property_id)
+    {
+    default:
+      G_OBJECT_WARN_INVALID_PROPERTY_ID (object, property_id, pspec);
+    }
+}
+
+static void
+doc_examples_obj_class_init (DocExamplesObjClass *klass)
+{
+  GObjectClass *gobject_class = G_OBJECT_CLASS (klass);
+  GParamSpec *pspec;
+
+  gobject_class->get_property = doc_examples_obj_get_property;
+  gobject_class->set_property = doc_examples_obj_set_property;
+
+  /**
+   * DocExamplesObj::signal-example:
+   * @object:
+   * @int_param: a parameter of type int
+   * @float_param: a parameter of type float
+   *
+   * Emitted when the connection status on the account changes.
+   *
+   * The @dbus_error_name and @details parameters were present, but
+   * non-functional (always %NULL), in older versions. They have been
+   * available with their current behaviour since version 0.11.7.
+   *
+   * Since: 0.99
+   */
+  g_signal_new ("signal-example",
+      G_TYPE_FROM_CLASS (gobject_class),
+      G_SIGNAL_RUN_LAST,
+      0, NULL, NULL,
+      NULL,
+      G_TYPE_NONE, 2, G_TYPE_UINT, G_TYPE_UINT);
+
+  /**
+   * DocExamplesObj:property-example:
+   *
+   * This is an example of how to document a property.
+   *
+   * Type: GLib.HashTable(utf8,gint8)
+   * Transfer: container
+   */
+  pspec = g_param_spec_boxed ("property-example",
+                              "Example property",
+                              "A contained GHashTable",
+                              G_TYPE_HASH_TABLE,
+                              G_PARAM_READWRITE);
+  g_object_class_install_property (gobject_class,
+                                   1,
+                                   pspec);
+}
+
+static void
+doc_examples_obj_init (DocExamplesObj *self)
+{
+
+}
+
+DocExamplesObj *
+doc_examples_obj_new (void)
+{
+  return g_object_new (DOC_EXAMPLES_TYPE_OBJ, NULL);
+}
---
+++ tests/doctool/doc-examples-obj.h
@@ -0,0 +1,54 @@
+/* doc-examples-obj.h */
+
+#ifndef _DOC_EXAMPLES_OBJ_H
+#define _DOC_EXAMPLES_OBJ_H
+
+#include <glib-object.h>
+
+G_BEGIN_DECLS
+
+#define DOC_EXAMPLES_TYPE_OBJ doc_examples_obj_get_type()
+
+#define DOC_EXAMPLES_OBJ(obj) \
+  (G_TYPE_CHECK_INSTANCE_CAST ((obj), \
+  DOC_EXAMPLES_TYPE_OBJ, DocExamplesObj))
+
+#define DOC_EXAMPLES_OBJ_CLASS(klass) \
+  (G_TYPE_CHECK_CLASS_CAST ((klass), \
+  DOC_EXAMPLES_TYPE_OBJ, DocExamplesObjClass))
+
+#define DOC_EXAMPLES_IS_OBJ(obj) \
+  (G_TYPE_CHECK_INSTANCE_TYPE ((obj), \
+  DOC_EXAMPLES_TYPE_OBJ))
+
+#define DOC_EXAMPLES_IS_OBJ_CLASS(klass) \
+  (G_TYPE_CHECK_CLASS_TYPE ((klass), \
+  DOC_EXAMPLES_TYPE_OBJ))
+
+#define DOC_EXAMPLES_OBJ_GET_CLASS(obj) \
+  (G_TYPE_INSTANCE_GET_CLASS ((obj), \
+  DOC_EXAMPLES_TYPE_OBJ, DocExamplesObjClass))
+
+typedef struct _DocExamplesObj DocExamplesObj;
+typedef struct _DocExamplesObjClass DocExamplesObjClass;
+
+
+struct _DocExamplesObj
+{
+  GObject parent;
+
+
+};
+
+struct _DocExamplesObjClass
+{
+  GObjectClass parent_class;
+};
+
+GType doc_examples_obj_get_type (void) G_GNUC_CONST;
+
+DocExamplesObj *doc_examples_obj_new (void);
+
+G_END_DECLS
+
+#endif /* _DOC_EXAMPLES_OBJ_H */
