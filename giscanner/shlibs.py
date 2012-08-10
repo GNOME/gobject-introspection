9a1e0c63c13f3567e75553d2d07dd914d5b81287
Chun-wei Fan <fanchunwei@src.gnome.org>
2012-08-10 12:41:22 +0800

    giscanner/dumper.py: Use os.name instead of os.uname()[0]
    os.uname is not available universally, so use something that exists
    universally.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=681820

--- giscanner/shlibs.py
+++ giscanner/shlibs.py
@@ -69,7 +69,7 @@ def _resolve_non_libtool(options, binary, libraries):
     if not libraries:
         return []
 
-    if os.uname()[0] == 'OpenBSD':
+    if os.name == 'OpenBSD':
         # Hack for OpenBSD when using the ports' libtool which uses slightly
         # different directories to store the libraries in. So rewite binary.args[0]
         # by inserting '.libs/'.
