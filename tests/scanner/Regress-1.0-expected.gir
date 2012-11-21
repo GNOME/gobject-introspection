GNOME/gobject-introspection

commit 5153efd29e9a813a97d58905340c0ec4af04c5bd

Add more tests for boxed types
gjs needs a boxed type that cannot be trivially allocated but has also
a complex constructor.

https://bugzilla.gnome.org/show_bug.cgi?id=612033

tests/scanner/regress.c
@@ -1953,6 +1953,53 @@ G_DEFINE_BOXED_TYPE(RegressTestBoxedC,
                     regress_test_boxed_c_ref,
                     regress_test_boxed_c_unref);
 
+struct _RegressTestBoxedD {
+  char *a_string;
+  gint a_int;
+};
+
+RegressTestBoxedD *
+regress_test_boxed_d_new (const char *a_string, int a_int)
+{
+  RegressTestBoxedD *boxed;
+
+  boxed = g_slice_new (RegressTestBoxedD);
+  boxed->a_string = g_strdup (a_string);
+  boxed->a_int = a_int;
+
+  return boxed;
+}
+
+RegressTestBoxedD *
+regress_test_boxed_d_copy (RegressTestBoxedD *boxed)
+{
+  RegressTestBoxedD *ret;
+
+  ret = g_slice_new (RegressTestBoxedD);
+  ret->a_string = g_strdup (boxed->a_string);
+  ret->a_int = boxed->a_int;
+
+  return ret;
+}
+
+void
+regress_test_boxed_d_free (RegressTestBoxedD *boxed)
+{
+  g_free (boxed->a_string);
+  g_slice_free (RegressTestBoxedD, boxed);
+}
+
+int
+regress_test_boxed_d_get_magic (RegressTestBoxedD *boxed)
+{
+  return strlen (boxed->a_string) + boxed->a_int;
+}
+
+G_DEFINE_BOXED_TYPE(RegressTestBoxedD,
+                    regress_test_boxed_d,
+                    regress_test_boxed_d_copy,
+                    regress_test_boxed_d_free);
+
 G_DEFINE_TYPE(RegressTestObj, regress_test_obj, G_TYPE_OBJECT);
 
 enum

tests/scanner/regress.h
@@ -432,6 +432,16 @@ struct _RegressTestBoxedC
 GType regress_test_boxed_c_get_type (void);
 RegressTestBoxedC *regress_test_boxed_c_new (void);
 
+typedef struct _RegressTestBoxedD RegressTestBoxedD;
+
+GType regress_test_boxed_d_get_type (void);
+
+RegressTestBoxedD *regress_test_boxed_d_new (const char *a_string, int a_int);
+RegressTestBoxedD *regress_test_boxed_d_copy (RegressTestBoxedD *boxed);
+void regress_test_boxed_d_free (RegressTestBoxedD *boxed);
+
+int regress_test_boxed_d_get_magic (RegressTestBoxedD *boxed);
+
 /* gobject */
 #define REGRESS_TEST_TYPE_OBJ              (regress_test_obj_get_type ())
 #define REGRESS_TEST_OBJECT(object)        (G_TYPE_CHECK_INSTANCE_CAST ((object), REGRESS_TEST_TYPE_OBJ, RegressTestObj))

commit 21cec56f5aa24bf1288a6ef3ecd2d1d168bff827

Regress: Update expected gir for previous changes
Changes introduced by 5153efd29e9a813a97d58905340c0ec4af04c5bd

tests/scanner/Regress-1.0-expected.gir
@@ -271,6 +271,40 @@ use it should be.</doc>
         </return-value>
       </constructor>
     </record>
+    <record name="TestBoxedD"
+            c:type="RegressTestBoxedD"
+            glib:type-name="RegressTestBoxedD"
+            glib:get-type="regress_test_boxed_d_get_type"
+            c:symbol-prefix="test_boxed_d">
+      <constructor name="new" c:identifier="regress_test_boxed_d_new">
+        <return-value transfer-ownership="full">
+          <type name="TestBoxedD" c:type="RegressTestBoxedD*"/>
+        </return-value>
+        <parameters>
+          <parameter name="a_string" transfer-ownership="none">
+            <type name="utf8" c:type="const char*"/>
+          </parameter>
+          <parameter name="a_int" transfer-ownership="none">
+            <type name="gint" c:type="int"/>
+          </parameter>
+        </parameters>
+      </constructor>
+      <method name="copy" c:identifier="regress_test_boxed_d_copy">
+        <return-value transfer-ownership="full">
+          <type name="TestBoxedD" c:type="RegressTestBoxedD*"/>
+        </return-value>
+      </method>
+      <method name="free" c:identifier="regress_test_boxed_d_free">
+        <return-value transfer-ownership="none">
+          <type name="none" c:type="void"/>
+        </return-value>
+      </method>
+      <method name="get_magic" c:identifier="regress_test_boxed_d_get_magic">
+        <return-value transfer-ownership="none">
+          <type name="gint" c:type="int"/>
+        </return-value>
+      </method>
+    </record>
     <record name="TestBoxedPrivate"
             c:type="RegressTestBoxedPrivate"
             disguised="1">