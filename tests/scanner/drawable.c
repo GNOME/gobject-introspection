bb1ac9cf2dc450c5d44b1e39e3162ba06069f4a1
Jasper St. Pierre <jstpierre@mecheye.net>
2013-02-02 12:00:15 -0500

    tests: Move TestInherit into Regress
    https://bugzilla.gnome.org/show_bug.cgi?id=693097

--- tests/scanner/drawable.c
+++ tests/scanner/drawable.c
@@ -1,54 +1,54 @@
 /* -*- mode: C; c-file-style: "gnu"; indent-tabs-mode: nil; -*- */
 #include "drawable.h"
 
-G_DEFINE_ABSTRACT_TYPE (TestInheritDrawable, test_inherit_drawable, G_TYPE_OBJECT);
+G_DEFINE_ABSTRACT_TYPE (RegressTestInheritDrawable, regress_test_inherit_drawable, G_TYPE_OBJECT);
 
 static void
-test_inherit_drawable_class_init (TestInheritDrawableClass *klass)
+regress_test_inherit_drawable_class_init (RegressTestInheritDrawableClass *klass)
 {
 
 }
 
 static void
-test_inherit_drawable_init (TestInheritDrawable *drawable)
+regress_test_inherit_drawable_init (RegressTestInheritDrawable *drawable)
 {
 
 }
 
-void 
-test_inherit_drawable_do_foo (TestInheritDrawable *drawable, int x)
+void
+regress_test_inherit_drawable_do_foo (RegressTestInheritDrawable *drawable, int x)
 {
-  
+
 }
 
 /**
- * test_inherit_drawable_get_origin:
+ * regress_test_inherit_drawable_get_origin:
  * @drawable:
  * @x: (out):
  * @y: (out):
  */
 void
-test_inherit_drawable_get_origin (TestInheritDrawable *drawable, int *x, int *y)
+regress_test_inherit_drawable_get_origin (RegressTestInheritDrawable *drawable, int *x, int *y)
 {
   *x = 0;
   *y = 0;
 }
 
 /**
- * test_inherit_drawable_get_size:
+ * regress_test_inherit_drawable_get_size:
  * @drawable:
  * @width: (out):
  * @height: (out):
  */
 void
-test_inherit_drawable_get_size (TestInheritDrawable *drawable, guint *width, guint *height)
+regress_test_inherit_drawable_get_size (RegressTestInheritDrawable *drawable, guint *width, guint *height)
 {
   *width = 42;
   *height = 42;
 }
 
 void
-test_inherit_drawable_do_foo_maybe_throw (TestInheritDrawable *drawable, int x, GError **error)
+regress_test_inherit_drawable_do_foo_maybe_throw (RegressTestInheritDrawable *drawable, int x, GError **error)
 {
   if (x != 42)
     g_set_error(error, 0, 12, "The answer should be 42!");
--- tests/scanner/drawable.h
+++ tests/scanner/drawable.h
@@ -1,33 +1,33 @@
-#ifndef __TEST_INHERIT_DRAWABLE_H__
-#define __TEST_INHERIT_DRAWABLE_H__
+#ifndef __REGRESS_TEST_INHERIT_DRAWABLE_H__
+#define __REGRESS_TEST_INHERIT_DRAWABLE_H__
 
 #include <glib-object.h>
 
-typedef struct _TestInheritDrawable TestInheritDrawable;
-typedef struct _TestInheritDrawableClass TestInheritDrawableClass;
+typedef struct _RegressTestInheritDrawable RegressTestInheritDrawable;
+typedef struct _RegressTestInheritDrawableClass RegressTestInheritDrawableClass;
 
-struct _TestInheritDrawable
+struct _RegressTestInheritDrawable
 {
   GObject parent_instance;
 };
 
-struct _TestInheritDrawableClass
+struct _RegressTestInheritDrawableClass
 {
   GObjectClass parent_class;
 };
 
-GType test_inherit_drawable_get_type (void) G_GNUC_CONST;
+GType regress_test_inherit_drawable_get_type (void) G_GNUC_CONST;
 
-void test_inherit_drawable_do_foo (TestInheritDrawable *drawable, int x);
-void test_inherit_drawable_get_origin (TestInheritDrawable *drawable, int *x, int *y);
-void test_inherit_drawable_get_size (TestInheritDrawable *drawable, guint *width, guint *height);
-void test_inherit_drawable_do_foo_maybe_throw (TestInheritDrawable *drawable, int x, GError **error);
+void regress_test_inherit_drawable_do_foo (RegressTestInheritDrawable *drawable, int x);
+void regress_test_inherit_drawable_get_origin (RegressTestInheritDrawable *drawable, int *x, int *y);
+void regress_test_inherit_drawable_get_size (RegressTestInheritDrawable *drawable, guint *width, guint *height);
+void regress_test_inherit_drawable_do_foo_maybe_throw (RegressTestInheritDrawable *drawable, int x, GError **error);
 
-typedef struct _TestInheritPixmapObjectClass TestInheritPixmapObjectClass;
+typedef struct _RegressTestInheritPixmapObjectClass RegressTestInheritPixmapObjectClass;
 
-struct _TestInheritPixmapObjectClass
+struct _RegressTestInheritPixmapObjectClass
 {
-  TestInheritDrawableClass parent_class;
+  RegressTestInheritDrawableClass parent_class;
 };
 
-#endif /* __TEST_INHERIT_DRAWABLE_H__ */
+#endif /* __REGRESS_TEST_INHERIT_DRAWABLE_H__ */
