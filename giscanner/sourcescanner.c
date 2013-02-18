5aa1e8c8cb1d8dee3c214235634539ccd7eda8fe
Jasper St. Pierre <jstpierre@mecheye.net>
2013-02-18 16:28:17 -0500

    sourcescanner: Fix issues with double constants in the copy

--- giscanner/sourcescanner.c
+++ giscanner/sourcescanner.c
@@ -57,7 +57,7 @@ gi_source_symbol_copy (GISourceSymbol * symbol)
   if (symbol->const_int_set)
     new_symbol->const_int = symbol->const_int;
   else if (symbol->const_double_set)
-    new_symbol->const_double = symbol->const_double_set;
+    new_symbol->const_double = symbol->const_double;
   else if (symbol->const_string != NULL)
     new_symbol->const_string = g_strdup (symbol->const_string);
 
