3ec52d6198a8afb6360bf9985cee03cdcbfdfb63
Jasper St. Pierre <jstpierre@mecheye.net>
2013-03-28 16:14:48 -0400

    giscanner: Add a simple automatic sections file generator
    This is a very basic sections file generator, and isn't too smart.
    It's simply intended to be a base to build docs on, and will be used
    if the user doesn't provide a sections file when calling g-ir-doc-tool,
    for convenience purposes.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=699856

--- giscanner/docmain.py
+++ giscanner/docmain.py
@@ -22,6 +22,7 @@ import os
 import optparse
 
 from .docwriter import DocWriter
+from .sectionparser import generate_sections_file, write_sections_file
 from .transformer import Transformer
 
 
@@ -38,6 +39,9 @@ def doc_main(args):
     parser.add_option("", "--add-include-path",
                       action="append", dest="include_paths", default=[],
                       help="include paths for other GIR files")
+    parser.add_option("", "--write-sections-file",
+                      action="store_true", dest="write_sections",
+                      help="Generate and write out a sections file")
 
     options, args = parser.parse_args(args)
     if not options.output:
@@ -55,7 +59,14 @@ def doc_main(args):
     extra_include_dirs.extend(options.include_paths)
     transformer = Transformer.parse_from_gir(args[1], extra_include_dirs)
 
-    writer = DocWriter(transformer, options.language)
-    writer.write(options.output)
+    if options.write_sections:
+        sections_file = generate_sections_file(transformer)
+
+        fp = open(options.output, 'w')
+        write_sections_file(fp, sections_file)
+        fp.close()
+    else:
+        writer = DocWriter(transformer, options.language)
+        writer.write(options.output)
 
     return 0
--- giscanner/sectionparser.py
+++ giscanner/sectionparser.py
@@ -18,6 +18,8 @@
 #
 
 import re
+from . import ast
+from .utils import to_underscores
 
 
 class SectionsFile(object):
@@ -87,5 +89,62 @@ def parse_sections_file(lines):
             continue
 
         current_subsection.symbols.append(line)
+
+    return SectionsFile(sections)
+
+def write_sections_file(f, sections_file):
+    for section in sections_file.sections:
+        f.write("\n<SECTION>\n")
+        if section.file is not None:
+            f.write("<FILE>%s</FILE>\n" % (section.file, ))
+        if section.title is not None:
+            f.write("<TITLE>%s</TITLE>\n" % (section.title, ))
+        if section.includes is not None:
+            f.write("<INCLUDE>%s</INCLUDE>\n" % (section.includes, ))
+
+        is_first_subsection = True
+        for subsection in section.subsections:
+            if subsection.name is not None:
+                f.write("<SUBSECTION %s>\n" % (subsection.name, ))
+            elif not is_first_subsection:
+                f.write("\n<SUBSECTION>\n")
+
+            is_first_subsection = False
+
+            for symbol in subsection.symbols:
+                f.write(symbol + "\n")
+
+def generate_sections_file(transformer):
+    ns = transformer.namespace
+
+    sections = []
+
+    def new_section(file_, title):
+        section = Section()
+        section.file = file_
+        section.title = title
+        section.subsections.append(Subsection(None))
+        sections.append(section)
+        return section
+
+    def append_symbol(section, sym):
+        section.subsections[0].symbols.append(sym)
+
+    general_section = new_section("main", "Main")
+
+    for node in ns.itervalues():
+        if isinstance(node, ast.Function):
+            append_symbol(general_section, node.symbol)
+        elif isinstance(node, (ast.Class, ast.Interface)):
+            gtype_name = node.gtype_name
+            file_name = to_underscores(gtype_name).replace('_', '-').lower()
+            section = new_section(file_name, gtype_name)
+            append_symbol(section, gtype_name)
+            append_symbol(section, node.glib_type_struct.target_giname.replace('.', ''))
+
+            for meth in node.methods:
+                append_symbol(section, meth.symbol)
+            for meth in node.static_methods:
+                append_symbol(section, meth.symbol)
 
     return SectionsFile(sections)
