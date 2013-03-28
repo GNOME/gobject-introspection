GNOME/gobject-introspection

commit 9a1e0c63c13f3567e75553d2d07dd914d5b81287

giscanner/dumper.py: Use os.name instead of os.uname()[0]
os.uname is not available universally, so use something that exists
universally.

https://bugzilla.gnome.org/show_bug.cgi?id=681820

giscanner/shlibs.py
@@ -69,7 +69,7 @@ def _resolve_non_libtool(options, binary, libraries):
     if not libraries:
         return []
 
-    if os.uname()[0] == 'OpenBSD':
+    if os.name == 'OpenBSD':
         # Hack for OpenBSD when using the ports' libtool which uses slightly
         # different directories to store the libraries in. So rewite binary.args[0]
         # by inserting '.libs/'.

commit 49b80bb567115b95c1051d2f01da7679e4e08154

Unbreak giscanner on OpenBSD.
Commit 9a1e0c63c13f3567e75553d2d07dd914d5b81287 broke this as
os.name doesn't return 'OpenBSD', but 'posix'

https://bugzilla.gnome.org/show_bug.cgi?id=696765

giscanner/shlibs.py
@@ -68,7 +68,7 @@ def _resolve_non_libtool(options, binary, libraries):
     if not libraries:
         return []
 
-    if os.name == 'OpenBSD':
+    if platform.platform().startswith('OpenBSD'):
         # Hack for OpenBSD when using the ports' libtool which uses slightly
         # different directories to store the libraries in. So rewite binary.args[0]
         # by inserting '.libs/'.