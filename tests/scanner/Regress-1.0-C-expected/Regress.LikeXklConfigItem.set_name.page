GNOME/gobject-introspection

commit a09072bd1f75dfc7497ed599e03e331bff411fd4

tests: Add a struct with fixed-size gchar
See https://bugzilla.redhat.com/920595

tests/scanner/Regress-1.0-expected.gir
@@ -1946,6 +1946,27 @@ exposed to language bindings.</doc>
         <type name="gdouble" c:type="double"/>
       </field>
     </record>
+    <record name="LikeXklConfigItem" c:type="RegressLikeXklConfigItem">
+      <field name="name" writable="1">
+        <array zero-terminated="0" c:type="gchar" fixed-size="32">
+          <type name="gchar" c:type="gchar"/>
+        </array>
+      </field>
+      <method name="set_name"
+              c:identifier="regress_like_xkl_config_item_set_name">
+        <return-value transfer-ownership="none">
+          <type name="none" c:type="void"/>
+        </return-value>
+        <parameters>
+          <instance-parameter name="self" transfer-ownership="none">
+            <type name="LikeXklConfigItem" c:type="RegressLikeXklConfigItem*"/>
+          </instance-parameter>
+          <parameter name="name" transfer-ownership="none">
+            <type name="utf8" c:type="const char*"/>
+          </parameter>
+        </parameters>
+      </method>
+    </record>
     <constant name="MAXUINT64"
               value="18446744073709551615"
               c:type="REGRESS_MAXUINT64">

tests/scanner/regress.c
@@ -4007,3 +4007,11 @@ void
 regress_test_versioning (void)
 {
 }
+
+void
+regress_like_xkl_config_item_set_name (RegressLikeXklConfigItem *self,
+                                       const char *name)
+{
+  strncpy (self->name, name, sizeof (self->name) - 1);
+  self->name[sizeof(self->name)-1] = '\0';
+}

tests/scanner/regress.h
@@ -905,6 +905,13 @@ typedef struct {
 
 void regress_test_struct_fixed_array_frob (RegressTestStructFixedArray *str);
 
+typedef struct {
+  gchar name[32];
+} RegressLikeXklConfigItem;
+
+void regress_like_xkl_config_item_set_name (RegressLikeXklConfigItem *self,
+                                            const char *name);
+
 #define REGRESS_UTF8_CONSTANT "const \xe2\x99\xa5 utf8"
 
 #ifdef __GNUC__

commit df6abacef97c0f47fd64f92a8fc99c42f481ad73

tests: add missing expected generated documentation
a09072bd1f75dfc7497ed599e03e331bff411fd4 introduced new symbols in
tests/scanner/regress.[ch] but was missing the expected generated
documentation.

https://bugzilla.gnome.org/show_bug.cgi?id=698616

tests/scanner/Regress-1.0-C-expected/Regress.LikeXklConfigItem.page
@@ -1,0 +1,25 @@
+<?xml version="1.0"?>
+<page id="Regress.LikeXklConfigItem"
+      type="topic"
+      style="record"
+      xmlns="http://projectmallard.org/1.0/"
+      xmlns:api="http://projectmallard.org/experimental/api/"
+      xmlns:ui="http://projectmallard.org/1.0/ui/">
+  <info>
+    
+      <link xref="index" group="record" type="guide"/>
+    
+  </info>
+  <title>Regress.LikeXklConfigItem</title>
+  
+  
+  
+    
+  
+  
+  
+  
+  
+  
+  
+</page>

tests/scanner/Regress-1.0-C-expected/Regress.LikeXklConfigItem.set_name.page
@@ -1,0 +1,57 @@
+<?xml version="1.0"?>
+<page id="Regress.LikeXklConfigItem.set_name"
+      type="topic"
+      style="method"
+      xmlns="http://projectmallard.org/1.0/"
+      xmlns:api="http://projectmallard.org/experimental/api/"
+      xmlns:ui="http://projectmallard.org/1.0/ui/">
+  <info>
+    
+    <link xref="Regress.LikeXklConfigItem" group="method" type="guide"/>
+    <api:function>
+      <api:returns>
+        <api:type>void</api:type>
+      </api:returns>
+      <api:name>regress_like_xkl_config_item_set_name</api:name>
+      <api:arg>
+        <api:type>RegressLikeXklConfigItem*</api:type>
+        <api:name>self</api:name>
+      </api:arg>
+      <api:arg>
+        <api:type>const char*</api:type>
+        <api:name>name</api:name>
+      </api:arg>
+    </api:function>
+
+  </info>
+  <title>regress_like_xkl_config_item_set_name</title>
+  
+<synopsis><code mime="text/x-csrc">
+void regress_like_xkl_config_item_set_name (RegressLikeXklConfigItem* self,
+                                            const char* name);
+</code></synopsis>
+
+  
+    
+  
+  
+  
+  
+<terms>
+<item>
+<title><code>self</code></title>
+
+</item>
+<item>
+<title><code>name</code></title>
+
+</item>
+<item>
+<title><code>Returns</code></title>
+
+</item>
+</terms>
+
+  
+  
+</page>

tests/scanner/Regress-1.0-Gjs-expected/Regress.LikeXklConfigItem.page
@@ -1,0 +1,25 @@
+<?xml version="1.0"?>
+<page id="Regress.LikeXklConfigItem"
+      type="guide"
+      style="record"
+      xmlns="http://projectmallard.org/1.0/"
+      xmlns:api="http://projectmallard.org/experimental/api/"
+      xmlns:ui="http://projectmallard.org/1.0/ui/">
+  <info>
+    
+      <link xref="index" group="record" type="guide"/>
+    
+  </info>
+  <title>Regress.LikeXklConfigItem</title>
+  
+  
+  
+    
+  
+  
+  
+  
+  
+  
+  
+</page>

tests/scanner/Regress-1.0-Gjs-expected/Regress.LikeXklConfigItem.set_name.page
@@ -1,0 +1,46 @@
+<?xml version="1.0"?>
+<page id="Regress.LikeXklConfigItem.set_name"
+      type="topic"
+      style="method"
+      xmlns="http://projectmallard.org/1.0/"
+      xmlns:api="http://projectmallard.org/experimental/api/"
+      xmlns:ui="http://projectmallard.org/1.0/ui/">
+  <info>
+    
+    <link xref="Regress.LikeXklConfigItem" group="method" type="guide"/>
+    <api:function>
+      <api:returns>
+        <api:type>void</api:type>
+      </api:returns>
+      <api:name>regress_like_xkl_config_item_set_name</api:name>
+      <api:arg>
+        <api:type>String</api:type>
+        <api:name>name</api:name>
+      </api:arg>
+    </api:function>
+
+  </info>
+  <title>Regress.LikeXklConfigItem.prototype.set_name</title>
+  
+<synopsis><code mime="text/x-gjs">
+function set_name(name:String):void {
+    // Gjs wrapper for regress_like_xkl_config_item_set_name()
+}
+</code></synopsis>
+
+  
+    
+  
+  
+  
+  
+<terms>
+<item>
+<title><code>name</code></title>
+
+</item>
+</terms>
+
+  
+  
+</page>

tests/scanner/Regress-1.0-Python-expected/Regress.LikeXklConfigItem.page
@@ -1,0 +1,25 @@
+<?xml version="1.0"?>
+<page id="Regress.LikeXklConfigItem"
+      type="guide"
+      style="record"
+      xmlns="http://projectmallard.org/1.0/"
+      xmlns:api="http://projectmallard.org/experimental/api/"
+      xmlns:ui="http://projectmallard.org/1.0/ui/">
+  <info>
+    
+      <link xref="index" group="record" type="guide"/>
+    
+  </info>
+  <title>Regress.LikeXklConfigItem</title>
+  
+  
+  
+    
+  
+  
+  
+  
+  
+  
+  
+</page>

tests/scanner/Regress-1.0-Python-expected/Regress.LikeXklConfigItem.set_name.page
@@ -1,0 +1,55 @@
+<?xml version="1.0"?>
+<page id="Regress.LikeXklConfigItem.set_name"
+      type="topic"
+      style="method"
+      xmlns="http://projectmallard.org/1.0/"
+      xmlns:api="http://projectmallard.org/experimental/api/"
+      xmlns:ui="http://projectmallard.org/1.0/ui/">
+  <info>
+    
+    <link xref="Regress.LikeXklConfigItem" group="method" type="guide"/>
+    <api:function>
+      <api:returns>
+        <api:type>none</api:type>
+      </api:returns>
+      <api:name>regress_like_xkl_config_item_set_name</api:name>
+      <api:arg>
+        <api:type>Regress.LikeXklConfigItem</api:type>
+        <api:name>self</api:name>
+      </api:arg>
+      <api:arg>
+        <api:type>unicode</api:type>
+        <api:name>name</api:name>
+      </api:arg>
+    </api:function>
+
+  </info>
+  <title>Regress.LikeXklConfigItem.set_name</title>
+  
+<synopsis><code mime="text/x-python">
+@accepts(Regress.LikeXklConfigItem, unicode)
+@returns(none)
+def set_name(self, name):
+    # Python wrapper for regress_like_xkl_config_item_set_name()
+</code></synopsis>
+
+  
+    
+  
+  
+  
+  
+<terms>
+<item>
+<title><code>self</code></title>
+
+</item>
+<item>
+<title><code>name</code></title>
+
+</item>
+</terms>
+
+  
+  
+</page>