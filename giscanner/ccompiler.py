8dbf68dbcc37894705291d4b367a3f5a6fe6f447
Chun-wei Fan <fanchunwei@src.gnome.org>
2014-08-22 18:48:50 +0800

    giscanner/ccompiler.py: Initiate Distutils Compiler Instance
    This updates the __init__ constructor method of CCompiler so that distutils
    can be used, with some environment or user-supplied options to create the
    compiler instance that is appropriate with the platform that the giscanner
    scripts are being run on, and sets some specific environment for the
    compilers as necessary.  This also adds a check_is_msvc() method that will
    progressively replace calls in the other giscanner scripts that attempt to
    check for CC=cl (or so), where a part of which is done in this patch.  This
    is done for dumper.py as well as it needs to be updated in this patch to
    use the updated ccompiler.py which uses distutils to initiate the compiler
    instance.
    
    Also, as we have been using the --library option on Windows to pass in the
    library (not DLL) to deduce the correct DLL to link to in the introspection
    files for some time, we no longer need to make a copy of the library (.lib)
    to introspect that matches the <namespace>-<namespace_version>.lib
    convention, but use the libraries that were passed in with --library
    directly, so that we can link the dumper program during the introspection
    build.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=728313

--- giscanner/ccompiler.py
+++ giscanner/ccompiler.py
@@ -37,51 +37,85 @@ class CCompiler(object):
     compiler = None
     _cflags_no_deprecation_warnings = ''
 
-    def __init__(self, compiler_name=None):
-        pass
+    def __init__(self,
+                 environ=os.environ,
+                 osname=os.name,
+                 compiler_name=None):
 
-    def get_internal_link_flags(self,
-                                args,
-                                libtool,
-                                libraries,
-                                libpaths,
-                                pkgconfig_msvc_flags,
-                                ns,
-                                ns_version):
+        if osname == 'nt':
+            # The compiler used here on Windows may well not be
+            # the same compiler that was used to build Python,
+            # as the official Python binaries are built with
+            # Visual Studio
+            if compiler_name is None:
+                if environ.get('MSYSTEM') == 'MINGW32' or environ.get('MSYSTEM') == 'MINGW64':
+                    compiler_name = 'mingw32'
+                else:
+                    compiler_name = distutils.ccompiler.get_default_compiler()
+            if compiler_name != 'msvc' and \
+               compiler_name != 'mingw32':
+                raise SystemExit('Specified Compiler \'%s\' is unsupported.' % compiler_name)
+        else:
+            # XXX: Is it common practice to use a non-Unix compiler
+            #      class instance on non-Windows on platforms g-i supports?
+            compiler_name = distutils.ccompiler.get_default_compiler()
+
+        # Now, create the distutils ccompiler instance based on the info we have.
+        self.compiler = distutils.ccompiler.new_compiler(compiler=compiler_name)
+        customize_compiler(self.compiler)
+
+        # customize_compiler from distutils only does customization
+        # for 'unix' compiler type.  Also, avoid linking to msvcrxx.dll
+        # for MinGW builds as the dumper binary does not link to the
+        # Python DLL, but link to msvcrt.dll if necessary.
+        if isinstance(self.compiler, Mingw32CCompiler):
+            if self.compiler.dll_libraries != ['msvcrt']:
+                self.compiler.dll_libraries = []
+            if self.compiler.preprocessor is None:
+                self.compiler.preprocessor = self.compiler.compiler + ['-E']
+
+        if self.check_is_msvc():
+            # We trick distutils to believe that we are (always) using a
+            # compiler supplied by a Windows SDK, so that we avoid launching
+            # a new build environment to detect the compiler that is used to
+            # build Python itself, which is not desirable, so that we use the
+            # compiler commands (and env) as-is.
+            os.environ['DISTUTILS_USE_SDK'] = '1'
+            if 'MSSdk' not in os.environ:
+                if 'WindowsSDKDir' in os.environ:
+                    os.environ['MSSdk'] = os.environ.get('WindowsSDKDir')
+                elif os.environ.get('VCInstallDir'):
+                    os.environ['MSSdk'] = os.environ.get('VCInstallDir')
+
+            self.compiler_cmd = 'cl.exe'
+
+            self._cflags_no_deprecation_warnings = "-wd4996"
+        else:
+            if (isinstance(self.compiler, Mingw32CCompiler)):
+                self.compiler_cmd = self.compiler.compiler[0]
+            else:
+                self.compiler_cmd = ''.join(self.compiler.executables['compiler'])
+
+            self._cflags_no_deprecation_warnings = "-Wno-deprecated-declarations"
+
+    def get_internal_link_flags(self, args, libtool, libraries, libpaths):
         # An "internal" link is where the library to be introspected
         # is being built in the current directory.
 
         # Search the current directory first
         # (This flag is not supported nor needed for Visual C++)
-        if pkgconfig_msvc_flags == '':
+        if not self.check_is_msvc():
             args.append('-L.')
 
-        # https://bugzilla.gnome.org/show_bug.cgi?id=625195
-        if not libtool:
-            # We don't have -Wl,-rpath for Visual C++, and that's
-            # going to cause a problem.  Instead, link to internal
-            # libraries by deducing the .lib file name using
-            # the namespace name and version
-            if pkgconfig_msvc_flags:
-                if ns_version:
-                    args.append(str.lower(ns) +
-                                '-' +
-                                ns_version + '.lib')
-                else:
-                    args.append(str.lower(ns) + '.lib')
-            else:
+            # https://bugzilla.gnome.org/show_bug.cgi?id=625195
+            if not libtool:
                 args.append('-Wl,-rpath=.')
-
-        # Ensure libraries are always linked as we are going to use ldd to work
-        # out their names later
-        if not libtool and pkgconfig_msvc_flags == '':
-            args.append('-Wl,--no-as-needed')
+                args.append('-Wl,--no-as-needed')
 
         for library in libraries:
-            # Visual C++: We have the needed .lib files now, and we need to link
-            # to .lib files, not the .dll as the --library option specifies the
-            # .dll(s) the .gir file refers to
-            if pkgconfig_msvc_flags == '':
+            if self.check_is_msvc():
+                args.append(library + '.lib')
+            else:
                 if library.endswith(".la"):  # explicitly specified libtool library
                     args.append(library)
                 else:
@@ -90,7 +124,7 @@ class CCompiler(object):
         for library_path in libpaths:
             # Not used/needed on Visual C++, and -Wl,-rpath options
             # will cause grief
-            if pkgconfig_msvc_flags == '':
+            if not self.check_is_msvc():
                 args.append('-L' + library_path)
                 if os.path.isabs(library_path):
                     if libtool:
@@ -99,15 +133,15 @@ class CCompiler(object):
                     else:
                         args.append('-Wl,-rpath=' + library_path)
 
-    def get_external_link_flags(self, args, libraries, pkgconfig_msvc_flags):
+    def get_external_link_flags(self, args, libraries):
         # An "external" link is where the library to be introspected
         # is installed on the system; this case is used for the scanning
         # of GLib in gobject-introspection itself.
 
         for library in libraries:
-            # The --library option on Windows pass in the .dll file(s) the
-            # .gir files refer to, so don't link to them on Visual C++
-            if pkgconfig_msvc_flags == '':
+            if self.check_is_msvc():
+                args.append(library + '.lib')
+            else:
                 if library.endswith(".la"):  # explicitly specified libtool library
                     args.append(library)
                 else:
@@ -115,15 +149,13 @@ class CCompiler(object):
 
     def resolve_windows_libs(self, libraries, options):
         args = []
-        compiler_cmd = os.environ.get('CC', 'cc')
         libsearch = []
 
         # When we are using Visual C++...
-        if 'cl.exe' in compiler_cmd or 'cl' in compiler_cmd:
+        if self.check_is_msvc():
             # The search path of the .lib's on Visual C++
             # is dependent on the LIB environmental variable,
             # so just query for that
-            is_msvc = True
             libpath = os.environ.get('LIB')
             libsearch = libpath.split(';')
 
@@ -135,7 +167,6 @@ class CCompiler(object):
 
         # When we are not using Visual C++ (i.e. we are using GCC)...
         else:
-            is_msvc = False
             libtool = utils.get_libtool_command(options)
             if libtool:
                 args.append(utils.which(os.environ.get('SHELL', 'sh.exe')))
@@ -143,7 +174,7 @@ class CCompiler(object):
                 args.append('--mode=execute')
             # FIXME: it could have prefix (i686-w64-mingw32-dlltool.exe)
             args.extend(['dlltool.exe', '--identify'])
-            proc = subprocess.Popen([compiler_cmd, '-print-search-dirs'],
+            proc = subprocess.Popen([self.compiler_cmd, '-print-search-dirs'],
                                     stdout=subprocess.PIPE)
             o, e = proc.communicate()
             for line in o.splitlines():
@@ -175,7 +206,7 @@ class CCompiler(object):
                                                 stdout=subprocess.PIPE)
                         o, e = proc.communicate()
                         for line in o.splitlines():
-                            if is_msvc:
+                            if self.check_is_msvc():
                                 # On Visual Studio, dumpbin -symbols something.lib gives the
                                 # filename of DLL without the '.dll' extension that something.lib
                                 # links to, in the line that contains
@@ -201,3 +232,9 @@ class CCompiler(object):
                 "ERROR: can't resolve libraries to shared libraries: " +
                 ", ".join(not_resolved))
         return shlibs
+
+    def check_is_msvc(self):
+        if isinstance(self.compiler, MSVCCompiler):
+            return True
+        else:
+            return False
