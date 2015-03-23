72b6df60f2d6bcd3628f57c794430f1eec4a04aa
Nicola Fontana <ntd@entidi.it>
2015-03-23 15:57:14 +0100

    Allow overriding of dlltool name
    Allow to override it with the DLLTOOL environment variable, leaving
    "dlltool" as a fallback when not defined so backward compatibility is
    ensured.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=761984

--- giscanner/ccompiler.py
+++ giscanner/ccompiler.py
@@ -281,8 +281,7 @@ class CCompiler(object):
                     args.append(utils.which(os.environ.get('SHELL', 'sh.exe')))
                 args.extend(libtool)
                 args.append('--mode=execute')
-            # FIXME: it could have prefix (i686-w64-mingw32-dlltool.exe)
-            args.extend(['dlltool.exe', '--identify'])
+            args.extend([os.environ.get('DLLTOOL', 'dlltool.exe'), '--identify'])
             proc = subprocess.Popen([self.compiler_cmd, '-print-search-dirs'],
                                     stdout=subprocess.PIPE)
             o, e = proc.communicate()
