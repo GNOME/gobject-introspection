GNOME/gobject-introspection

commit 09daa28c5b4625b6f274b3b340bc85e33bef80b5

tests: Add regression test for (transfer full) returning callbacks
https://bugzilla.gnome.org/show_bug.cgi?id=750286

tests/scanner/regress.c
@@ -3438,6 +3438,20 @@ regress_test_callback_user_data (RegressTestCallbackUserData callback,
   return callback(user_data);
 }
 
+/**
+ * regress_test_callback_return_full:
+ * @callback: (scope call):
+ *
+ **/
+void
+regress_test_callback_return_full (RegressTestCallbackReturnFull callback)
+{
+  RegressTestObj *obj;
+
+  obj = callback ();
+  g_object_unref (obj);
+}
+
 static GSList *notified_callbacks = NULL;
 
 /**

tests/scanner/regress.h
@@ -1042,6 +1042,11 @@ typedef void (*RegressTestCallbackOwnedGError) (GError *error);
  */
 typedef int (*RegressTestCallbackFull) (int foo, double bar, char *path);
 /**
+ * RegressTestCallbackReturnFull:
+ * Return value: (transfer full):
+ */
+typedef RegressTestObj *(*RegressTestCallbackReturnFull) (void);
+/**
  * RegressTestCallbackArray:
  * @one: (array length=one_length):
  * @one_length:
@@ -1074,6 +1079,9 @@ int regress_test_array_inout_callback (RegressTestCallbackArrayInOut callback);
 _GI_TEST_EXTERN
 int regress_test_callback_user_data (RegressTestCallbackUserData callback,
                              gpointer user_data);
+
+_GI_TEST_EXTERN
+void regress_test_callback_return_full (RegressTestCallbackReturnFull callback);
 
 _GI_TEST_EXTERN
 int regress_test_callback_destroy_notify (RegressTestCallbackUserData callback,

commit 254ac953c6b18a0d4e93781cd9c29bf3dcd4953a

tests: fix make distcheck
Add missing doc-tool tests, fixes "make distcheck" breakage
introduced by 09daa28c5b4625b6f274b3b340bc85e33bef80b5

tests/scanner/Regress-1.0-C-expected/Regress.TestCallbackReturnFull.page
@@ -1,0 +1,24 @@
+<?xml version="1.0"?>
+<page id="Regress.TestCallbackReturnFull"
+      type="topic"
+      style="callback"
+      xmlns="http://projectmallard.org/1.0/"
+      xmlns:api="http://projectmallard.org/experimental/api/"
+      xmlns:ui="http://projectmallard.org/1.0/ui/">
+  <info>
+    <link xref="index" group="callback" type="guide"/>
+    </info>
+  <title>Regress.TestCallbackReturnFull</title>
+  <synopsis><code mime="text/x-csrc">
+RegressTestObj* TestCallbackReturnFull (void);
+  </code></synopsis>
+
+
+<terms>
+<item>
+<title><code>Returns</code></title>
+
+</item>
+</terms>
+
+</page>

tests/scanner/Regress-1.0-C-expected/Regress.test_callback_return_full.page
@@ -1,0 +1,38 @@
+<?xml version="1.0"?>
+<page id="Regress.test_callback_return_full"
+      type="topic"
+      style="function"
+      xmlns="http://projectmallard.org/1.0/"
+      xmlns:api="http://projectmallard.org/experimental/api/"
+      xmlns:ui="http://projectmallard.org/1.0/ui/">
+  <info>
+    <link xref="index" group="function" type="guide"/>
+    <api:function>
+      <api:returns>
+        <api:type>void</api:type>
+      </api:returns>
+      <api:name>regress_test_callback_return_full</api:name>
+      <api:arg>
+        <api:type>RegressTestCallbackReturnFull</api:type>
+        <api:name>callback</api:name>
+      </api:arg>
+    </api:function>
+  </info>
+  <title>regress_test_callback_return_full</title>
+  <synopsis><code mime="text/x-csrc">
+void regress_test_callback_return_full (RegressTestCallbackReturnFull callback);
+  </code></synopsis>
+
+
+<terms>
+<item>
+<title><code>callback</code></title>
+
+</item>
+<item>
+<title><code>Returns</code></title>
+
+</item>
+</terms>
+
+</page>

tests/scanner/Regress-1.0-Gjs-expected/Regress.TestCallbackReturnFull.page
@@ -1,0 +1,25 @@
+<?xml version="1.0"?>
+<page id="Regress.TestCallbackReturnFull"
+      type="topic"
+      style="callback"
+      xmlns="http://projectmallard.org/1.0/"
+      xmlns:api="http://projectmallard.org/experimental/api/"
+      xmlns:ui="http://projectmallard.org/1.0/ui/">
+  <info>
+    <link xref="index" group="callback" type="guide"/>
+  </info>
+  <title>Regress.TestCallbackReturnFull</title>
+  <synopsis><code mime="text/x-gjs">
+function onTestCallbackReturnFull(): <link xref="Regress.TestObj">Regress.TestObj</link> {
+}
+  </code></synopsis>
+
+
+<terms>
+<item>
+<title><code>Returns</code></title>
+
+</item>
+</terms>
+
+</page>

tests/scanner/Regress-1.0-Gjs-expected/Regress.test_callback_return_full.page
@@ -1,0 +1,36 @@
+<?xml version="1.0"?>
+<page id="Regress.test_callback_return_full"
+      type="topic"
+      style="function"
+      xmlns="http://projectmallard.org/1.0/"
+      xmlns:api="http://projectmallard.org/experimental/api/"
+      xmlns:ui="http://projectmallard.org/1.0/ui/">
+  <info>
+    <link xref="index" group="function" type="guide"/>
+    <api:function>
+      <api:returns>
+        <api:type>void</api:type>
+      </api:returns>
+      <api:name>regress_test_callback_return_full</api:name>
+      <api:arg>
+        <api:type>Regress.TestCallbackReturnFull</api:type>
+        <api:name>callback</api:name>
+      </api:arg>
+    </api:function>
+  </info>
+  <title>Regress.test_callback_return_full</title>
+  <synopsis><code mime="text/x-gjs">
+function test_callback_return_full(callback: <link xref="Regress.TestCallbackReturnFull">Regress.TestCallbackReturnFull</link>): void {
+    // Gjs wrapper for regress_test_callback_return_full()
+}
+  </code></synopsis>
+
+
+<terms>
+<item>
+<title><code>callback</code></title>
+
+</item>
+</terms>
+
+</page>

tests/scanner/Regress-1.0-Python-expected/Regress.TestCallbackReturnFull.page
@@ -1,0 +1,25 @@
+<?xml version="1.0"?>
+<page id="Regress.TestCallbackReturnFull"
+      type="topic"
+      style="callback"
+      xmlns="http://projectmallard.org/1.0/"
+      xmlns:api="http://projectmallard.org/experimental/api/"
+      xmlns:ui="http://projectmallard.org/1.0/ui/">
+  <info>
+    <link xref="index" group="callback" type="guide"/>
+  </info>
+  <title>Regress.TestCallbackReturnFull</title>
+  <synopsis><code mime="text/x-python">
+@returns(Regress.TestObj)
+def on_TestCallbackReturnFull():
+  </code></synopsis>
+
+
+<terms>
+<item>
+<title><code>Returns</code></title>
+
+</item>
+</terms>
+
+</page>

tests/scanner/Regress-1.0-Python-expected/Regress.test_callback_return_full.page
@@ -1,0 +1,37 @@
+<?xml version="1.0"?>
+<page id="Regress.test_callback_return_full"
+      type="topic"
+      style="function"
+      xmlns="http://projectmallard.org/1.0/"
+      xmlns:api="http://projectmallard.org/experimental/api/"
+      xmlns:ui="http://projectmallard.org/1.0/ui/">
+  <info>
+    <link xref="index" group="function" type="guide"/>
+    <api:function>
+      <api:returns>
+        <api:type>none</api:type>
+      </api:returns>
+      <api:name>regress_test_callback_return_full</api:name>
+      <api:arg>
+        <api:type>Regress.TestCallbackReturnFull</api:type>
+        <api:name>callback</api:name>
+      </api:arg>
+    </api:function>
+  </info>
+  <title>Regress.test_callback_return_full</title>
+  <synopsis><code mime="text/x-python">
+@accepts(Regress.TestCallbackReturnFull)
+@returns(none)
+def test_callback_return_full(callback):
+    # Python wrapper for regress_test_callback_return_full()
+  </code></synopsis>
+
+
+<terms>
+<item>
+<title><code>callback</code></title>
+
+</item>
+</terms>
+
+</page>

tests/scanner/Regress-1.0-expected.gir
@@ -2391,6 +2391,12 @@ use it should be.</doc>
         </parameter>
       </parameters>
     </callback>
+    <callback name="TestCallbackReturnFull"
+              c:type="RegressTestCallbackReturnFull">
+      <return-value transfer-ownership="full">
+        <type name="TestObj" c:type="RegressTestObj*"/>
+      </return-value>
+    </callback>
     <callback name="TestCallbackUserData" c:type="RegressTestCallbackUserData">
       <return-value transfer-ownership="none">
         <type name="gint" c:type="int"/>
@@ -5353,6 +5359,18 @@ condition in bindings which needs to be tested.</doc>
         </parameter>
         <parameter name="notify" transfer-ownership="none" scope="async">
           <type name="GLib.DestroyNotify" c:type="GDestroyNotify"/>
+        </parameter>
+      </parameters>
+    </function>
+    <function name="test_callback_return_full"
+              c:identifier="regress_test_callback_return_full">
+      <return-value transfer-ownership="none">
+        <type name="none" c:type="void"/>
+      </return-value>
+      <parameters>
+        <parameter name="callback" transfer-ownership="none" scope="call">
+          <type name="TestCallbackReturnFull"
+                c:type="RegressTestCallbackReturnFull"/>
         </parameter>
       </parameters>
     </function>

tests/scanner/Regress-1.0-sections-expected.txt
@@ -87,6 +87,7 @@ regress_test_callback
 regress_test_callback_async
 regress_test_callback_destroy_notify
 regress_test_callback_destroy_notify_no_user_data
+regress_test_callback_return_full
 regress_test_callback_thaw_async
 regress_test_callback_thaw_notifications
 regress_test_callback_user_data