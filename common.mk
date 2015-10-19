9d6d00b0c0268b80413f1998d9b5ecbb07fb05d8
Alexander Kanavin <alex.kanavin@gmail.com>
2015-10-19 18:29:21 +0300

    configure.ac: add host-gi, gi-cross-wrapper, gi-ldd-wrapper and introspection-data options
    With the first option, gobject-introspection tools (g-ir-doc-tool and g-ir-scanner)
    that are already installed in the host system will be used for building the source tree.
    
    With the second option, g-ir-scanner will be instructed to use an executable
    wrapper to run binaries it's producing, and g-ir-compiler will be run
    through the same wrapper (host system's g-ir-compiler cannot be used because
    it's producing architecture-specific output).
    
    With the third option, giscanner will be instructed to use a special ldd
    command instead of system's ldd (which does not work when the binary to inspect
    is compiled for a different architecture).
    
    With the fourth option, it is possible to disable building of introspection data
    (.gir and .typelib files), which may be difficult or impossible in cross-compilation
    environments, because of lack of emulation (or native hardware) for the target architecture
    on which the target binaries can be run.
    
    These options are useful when cross-compiling for a different target architecture.
    
    Signed-off-by: Alexander Kanavin <alex.kanavin@gmail.com>

--- common.mk
+++ common.mk
@@ -6,6 +6,15 @@
 # module itself.
 #
 
+if USE_HOST_GI
+INTROSPECTION_SCANNER = \
+    env PATH="$(PATH)" \
+        LPATH=.libs \
+        CC="$(CC)" \
+        PYTHONPATH=$(top_builddir):$(top_srcdir) \
+        UNINSTALLED_INTROSPECTION_BUILDDIR=$(top_builddir) \
+        g-ir-scanner
+else
 INTROSPECTION_SCANNER = \
     env PATH=".libs:$(PATH)" \
         LPATH=.libs \
@@ -14,9 +23,24 @@ INTROSPECTION_SCANNER = \
         UNINSTALLED_INTROSPECTION_SRCDIR=$(top_srcdir) \
         UNINSTALLED_INTROSPECTION_BUILDDIR=$(top_builddir) \
         $(top_builddir)/g-ir-scanner
+endif
+
+if USE_CROSS_WRAPPER
+CROSS_WRAPPER_ARG = --use-binary-wrapper=$(GI_CROSS_WRAPPER)
+else
+CROSS_WRAPPER_ARG =
+endif
+
+if USE_LDD_WRAPPER
+LDD_WRAPPER_ARG = --use-ldd-wrapper=$(GI_LDD_WRAPPER)
+else
+LDD_WRAPPER_ARG =
+endif
 
 INTROSPECTION_SCANNER_ARGS = \
     --verbose \
+    $(CROSS_WRAPPER_ARG) \
+    $(LDD_WRAPPER_ARG) \
     -I$(top_srcdir) \
     --add-include-path=$(srcdir) \
     --add-include-path=$(top_srcdir)/gir \
@@ -24,9 +48,15 @@ INTROSPECTION_SCANNER_ARGS = \
     --add-include-path=$(top_builddir) \
     --add-include-path=$(top_builddir)/gir
 
+if USE_CROSS_WRAPPER
+INTROSPECTION_COMPILER = \
+    env PATH=".libs:$(PATH)" \
+        $(GI_CROSS_WRAPPER) $(top_builddir)/.libs/g-ir-compiler$(EXEEXT)
+else
 INTROSPECTION_COMPILER = \
     env PATH=".libs:$(PATH)" \
         $(top_builddir)/g-ir-compiler$(EXEEXT)
+endif
 
 INTROSPECTION_COMPILER_ARGS = \
     --includedir=$(srcdir) \
@@ -35,6 +65,14 @@ INTROSPECTION_COMPILER_ARGS = \
     --includedir=$(top_builddir) \
     --includedir=$(top_builddir)/gir
 
+if USE_HOST_GI
+INTROSPECTION_DOCTOOL = \
+    env PATH="$(PATH)" \
+        LPATH=.libs \
+        PYTHONPATH=$(top_builddir):$(top_srcdir) \
+        UNINSTALLED_INTROSPECTION_BUILDDIR=$(top_builddir) \
+        g-ir-doc-tool
+else
 INTROSPECTION_DOCTOOL = \
     env PATH=".libs:$(PATH)" \
         LPATH=.libs \
@@ -42,6 +80,7 @@ INTROSPECTION_DOCTOOL = \
         UNINSTALLED_INTROSPECTION_SRCDIR=$(top_srcdir) \
         UNINSTALLED_INTROSPECTION_BUILDDIR=$(top_builddir) \
         $(top_builddir)/g-ir-doc-tool
+endif
 
 INTROSPECTION_DOCTOOL_ARGS = \
     --add-include-path=$(srcdir) \
