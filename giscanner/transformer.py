21b07858d25785666598a654636b46ba3efea826
Philip Chimento <philip.chimento@gmail.com>
2015-12-01 23:00:53 -0800

    transformer: Strip whitespace from filter commands
    On OS X, the output of sed as reported back to Python has a newline at
    the end. It seems like a good idea to strip whitespace from the symbol
    and identifier filter commands anyhow, so strip() the result.
    
    See #139.

--- giscanner/transformer.py
+++ giscanner/transformer.py
@@ -267,6 +267,7 @@ currently-scanned namespace is first."""
                 raise ValueError('filter: "%s" exited: %d with error: %s' %
                                  (self._symbol_filter_cmd, proc.returncode, err))
             name = proc_name.decode('ascii')
+            name = name.strip()
 
         matches = []  # Namespaces which might contain this name
         unprefixed_namespaces = []  # Namespaces with no prefix, last resort
@@ -331,7 +332,7 @@ raise ValueError."""
             if proc.returncode:
                 raise ValueError('filter: "%s" exited: %d with error: %s' %
                                  (self._identifier_filter_cmd, proc.returncode, err))
-            ident = proc_ident.decode('ascii')
+            ident = proc_ident.decode('ascii').strip()
 
         hidden = ident.startswith('_')
         if hidden:
