57d59d549dd0045c0a92e503a21e99827fb71a51
Chun-wei Fan <fanchunwei@src.gnome.org>
2018-06-07 16:55:21 +0800

    build: Drop Visual Studio projects
    We now have Meson build files which can handle builds using Visual
    Studio now, so we should focus maintenance efforts on the build system
    for Visual Studio on Meson.
    
    This will also allow build options to be more flexible, and has the
    advantage of being able to build directly out of a GIT checkout, without
    needing to generate source dist tarballs.

--- win32/gi_msvc_build_utils.py
+++
@@ -1,8 +0,0 @@
-import os
-
-def parent_dir(path):
-    if not os.path.isabs(path):
-        path = os.path.abspath(path)
-    if os.path.isfile(path):
-        path = os.path.dirname(path)
-    return os.path.split(path)[0]
