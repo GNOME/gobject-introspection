6c85b1c45460edd74395b119ebbff6a53eacec7e
Christoph Reiter <reiter.christoph@gmail.com>
2018-07-26 19:11:51 +0200

    tests: port offset tests to Python
    Use the test executable to generate files in a temporary location
    and use difflib to compare their content.
    
    This removes the requirement on "diff" for the test.

---
+++ tests/offsets/test_offsets.py
@@ -0,0 +1,47 @@
+import io
+import os
+import tempfile
+import difflib
+import contextlib
+import subprocess
+import unittest
+
+
+@contextlib.contextmanager
+def temp_filename(*args, **kwargs):
+    fd, name = tempfile.mkstemp(*args, **kwargs)
+    try:
+        os.close(fd)
+        yield name
+    finally:
+        os.unlink(name)
+
+
+class TestOffsets(unittest.TestCase):
+
+    def test_main(self):
+        gitestoffsets = os.path.join(os.environ["builddir"], "gitestoffsets")
+
+        env = os.environ.copy()
+        env["GI_TYPELIB_PATH"] = os.pathsep.join(
+            [os.environ["builddir"], os.environ["top_builddir"]])
+
+        with temp_filename(suffix=".compiled") as compiled_name, \
+                temp_filename(suffix=".introspected") as introspected_name:
+
+            subprocess.check_call(
+                [gitestoffsets, compiled_name, introspected_name], env=env)
+
+            with io.open(compiled_name, encoding="utf-8") as compiled, \
+                    io.open(introspected_name, encoding="utf-8") as introspected:
+
+                result = difflib.unified_diff(
+                    compiled.readlines(), introspected.readlines(),
+                    fromfile=compiled_name, tofile=introspected_name)
+                result = "".join(result)
+                if result:
+                    raise AssertionError(result)
+
+
+if __name__ == '__main__':
+    unittest.main()
