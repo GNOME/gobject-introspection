61cf8a574dd2015b697c7decc37558739837526b
Christoph Reiter <reiter.christoph@gmail.com>
2018-11-22 07:36:57 +0100

    gitlab-ci: Update the docker image to use fedora 29 and the latest meson

--- .gitlab-ci/run-docker.sh
+++ .gitlab-ci/run-docker.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-TAG="registry.gitlab.gnome.org/gnome/gobject-introspection:v2"
+TAG="registry.gitlab.gnome.org/gnome/gobject-introspection:v3"
 
 sudo docker build --build-arg HOST_USER_ID="$UID" --tag "${TAG}" \
     --file "Dockerfile" .
