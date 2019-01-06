90121df20ddf18afdb1c25212035748ff54519c5
Christoph Reiter <reiter.christoph@gmail.com>
2019-01-06 15:50:14 +0100

    ci: use the pkg-config.exe hosted by meson since sourceforge is flaky

--- .gitlab-ci/test-msvc.bat
+++ .gitlab-ci/test-msvc.bat
@@ -5,10 +5,9 @@ call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary
 py -3 -c "import urllib.request, sys; urllib.request.urlretrieve(*sys.argv[1:])" "https://github.com/lexxmark/winflexbison/releases/download/v2.5.14/win_flex_bison-2.5.14.zip" win_flex_bison.zip
 py -3 -m zipfile -e win_flex_bison.zip win_flex_bison
 
-py -3 -c "import urllib.request, sys; urllib.request.urlretrieve(*sys.argv[1:])" "https://sourceforge.net/projects/pkgconfiglite/files/0.28-1/pkg-config-lite-0.28-1_bin-win32.zip/download" pkg-config-lite-0.28-1.zip
-py -3 -m zipfile -e pkg-config-lite-0.28-1.zip .
+py -3 -c "import urllib.request, sys; urllib.request.urlretrieve(*sys.argv[1:])" "https://nirbheek.in/files/binaries/pkg-config/win32/pkg-config.exe" pkg-config.exe
 
-SET PATH=%CD%\pkg-config-lite-0.28-1\bin;%CD%\win_flex_bison;%PATH%
+SET PATH=%CD%;%CD%\win_flex_bison;%PATH%
 
 pip3 install --upgrade --user meson==0.49.0 || goto :error
 meson _build || goto :error
