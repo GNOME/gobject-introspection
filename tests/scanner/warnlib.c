1857a816a8ac8679a32452029ad842b166c01176
Philip Chimento <philip.chimento@gmail.com>
2019-04-15 21:26:24 -0700

    tests: Mark unused function arguments with G_GNUC_UNUSED
    These files are exported for other projects to compile, so they should
    compile with as few compiler warnings as possible. If GJS or PyGObject
    has -Wunused-parameter turned on, then there should not be warnings in
    these files.

--- tests/scanner/warnlib.c
+++ tests/scanner/warnlib.c
@@ -22,7 +22,7 @@ typedef WarnLibWhateverIface WarnLibWhateverInterface;
 G_DEFINE_INTERFACE (WarnLibWhatever, warnlib_whatever, G_TYPE_OBJECT)
 
 static void
-warnlib_whatever_default_init(WarnLibWhateverIface *iface)
+warnlib_whatever_default_init(WarnLibWhateverIface *iface G_GNUC_UNUSED)
 {
 }
 
