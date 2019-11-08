3c204fdb97a4dc2fce0a14388e08daeaa8560dfd
Christoph Reiter <reiter.christoph@gmail.com>
2019-11-08 16:41:29 +0100

    build: require meson 0.50.1
    See !186

--- .gitlab-ci/test-msys2-meson.sh
+++ .gitlab-ci/test-msys2-meson.sh
@@ -31,7 +31,7 @@ pacman --noconfirm -S --needed \
 export CCACHE_BASEDIR="${CI_PROJECT_DIR}"
 export CCACHE_DIR="${CCACHE_BASEDIR}/_ccache"
 
-pip3 install --upgrade --user meson==0.49.2 flake8
+pip3 install --upgrade --user meson==0.50.1 flake8
 export PATH="$HOME/.local/bin:$PATH"
 
 # FIXME: https://github.com/Alexpux/MINGW-packages/pull/4064
