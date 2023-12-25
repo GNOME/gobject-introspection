fb6f33082a42202c55dc3d5cbc984cc9b6b01629
Emmanuele Bassi <ebassi@gnome.org>
2023-12-25 00:06:53 +0000

    tests: Do not use PYTHONPATH to import giscanner
    The PYTHONPATH environment variable will prepend its contents to
    sys.path; since giscanner contains an ast sub-module, we are going to
    cause a collision with Python's own ast module. In some cases, Python
    3.12's distutils compatibility shim will try to import Python's ast,
    which will end up trying to import giscanner.ast instead.
    
    Instead of prepending the giscanner build directory, we can append it,
    and keep the current project layout.
    
    See: #429

--- tests/warn/warningtester.py
+++ tests/warn/warningtester.py
@@ -11,6 +11,15 @@ sys.path.insert(0, path)
 builtins.__dict__['DATADIR'] = path
 builtins.__dict__['GIR_DIR'] = path
 
+# We cannot use PYTHONPATH, because it would prepend the giscanner
+# root, and we have an "ast" module that conflicts with Python's
+# own ast. In some cases, Python's distutils ends up importing ast,
+# and that will end up trying to import giscanner.ast
+path = os.getenv('GISCANNER_DIR', None)
+assert path is not None
+for p in path.split(os.pathsep):
+    sys.path.append(p)
+
 from giscanner.annotationparser import GtkDocCommentBlockParser
 from giscanner.ast import Include, Namespace
 from giscanner.introspectablepass import IntrospectablePass
