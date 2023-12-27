233dd838b6dc926fa37d4b047fce9a317c8142e5
Emmanuele Bassi <ebassi@gnome.org>
2023-12-27 19:05:24 +0000

    Validate the GIR files against their schema
    We want to ensure that any change we make to the GIR format is actually
    tested against the schema, so we don't leave other consumers of the
    introspection data in the lurch.
    
    The requirements for this functionality are:
    
    - lxml, for the validating parser
    - rnc2rng, for the RelaxNG Compact to RelaxNG schema conversion

---
+++ misc/validate-gir.py
@@ -0,0 +1,21 @@
+import argparse
+import os
+import sys
+
+from lxml import etree
+
+
+args = argparse.ArgumentParser(prog="validate-gir")
+args.add_argument("-S", "--schema", metavar="FILE", help="the schema file", required=True)
+args.add_argument("infile", metavar="GIRFILE", type=argparse.FileType('r', encoding='UTF-8'),
+                  default=sys.stdin, help="the GIR file to validate")
+
+options = args.parse_args(sys.argv[1:])
+
+with open(options.schema, "r", encoding="utf-8") as f:
+    schema = etree.RelaxNG.from_rnc_string(f.read())
+
+gir = etree.parse(options.infile)
+if not schema.validate(gir):
+    print(f"Validation errors: {schema.error_log}", file=sys.stderr)
+    sys.exit(1)
