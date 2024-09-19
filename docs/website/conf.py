fb5b2bf6cb759516ca8ea041b51068153d339faa
FeRD (Frank Dana) <ferdnyc@gmail.com>
2024-09-18 20:46:31 -0400

    Sphinx: Add gitlab source-link anchor rewriting
    Inspired by the disabled `rewrite_github_anchors` in the Sphinx source,
    this rule specifically targets source-code links with `#L1234` or
    `#L1234-3456` style fragments. Since those fragments are "virtual"
    (do not correspond to IDs in the HTML), they cannot be checked by
    the link checker, so they're simply removed for validation purposes.

--- docs/website/conf.py
+++ docs/website/conf.py
@@ -1,5 +1,10 @@
 # -*- coding: utf-8 -*-
 
+import re
+from urllib.parse import urlparse, urlunparse
+
+from sphinx.application import Sphinx
+
 extensions = [
     'sphinx.ext.extlinks',
 ]
@@ -34,3 +39,18 @@ extlinks = {
         'https://gitlab.gnome.org/GNOME/gobject-introspection/-/merge_requests/%s', '!%s'),
     'user': ('https://gitlab.gnome.org/%s', None),
 }
+
+
+_FRAGMENT_MATCH = re.compile(r'L(\d+)(-\d*)?$')
+def rewrite_gitlab_source_anchors(app: Sphinx, uri: str):
+    parsed = urlparse(uri)
+    if (parsed.hostname in ["gitlab.gnome.org", "gitlab.com"]
+            and parsed.fragment
+            and _FRAGMENT_MATCH.match(parsed.fragment)):
+       return urlunparse(parsed._replace(fragment=''))
+    return None
+
+
+def setup(app: Sphinx):
+    app.connect('linkcheck-process-uri', rewrite_gitlab_source_anchors)
+
